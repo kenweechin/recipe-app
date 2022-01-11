b0a80e1441da2dc00c44b85bcc30c3c009c3210b
kenweechin <kenweechin@gmail.com>
2022-01-11 21:57:49 +0000

    Add profile page for the login user

--- app.py
+++ app.py
@@ -42,6 +42,8 @@ def register():
 
         session["user"] = request.form.get("username").lower()
         flash("Registration Completed!")
+        return redirect(url_for("profile", username=session["user"]))
+
     return render_template("register.html")
 
 
@@ -55,6 +57,7 @@ def login():
             if check_password_hash(existing_user["password"], request.form.get("password")):
                 session["user"] = request.form.get("username").lower()
                 flash("Welcome, {}".format(request.form.get("username")))
+                return redirect(url_for("profile", username=session["user"]))
             else:
                 flash("You've entered an invalid username and/or password")
                 return redirect(url_for("login"))
@@ -62,8 +65,14 @@ def login():
         else:
             flash("Sorry, incorrect username and/or password")
             return redirect(url_for("login"))
-            
+
     return render_template("login.html")
+
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    username = mongo.db.users.find_one({"username": session["user"]})["username"]
+    return render_template("profile.html", username=username)
 
 
 if __name__ == "__main__":
